a70627dcf2f60f01eca8130ccc2d2cc97478d649
Seoyeon Park <happy_tjdus@naver.com>
2022-08-18 18:15:48 +0900

    learned jQuery

--- colors.js
+++ colors.js
@@ -1,21 +1,17 @@
 var Body = {
     setColor:function(color){
-        document.querySelector('body').style.color=color;
+        // document.querySelector('body').style.color=color;
+        $('body').css('color', color);
     },
     setBackgroundColor:function(color){
-        document.querySelector('body').style.backgroundColor=color;
+        // document.querySelector('body').style.backgroundColor=color;
+        $('body').css('backgroundColor', color);
     }
 
 }
 var Links ={
     setColor:function(color){
-        var alist = document.querySelectorAll('a');
-        var i=0;
-        while(i<alist.length){
-        console.log(alist[i])
-        alist[i].style.color = color
-        i=i+1;
-        }
+        $('a').css('color', color);
     }
 }
 function nightDayHandler(self){
