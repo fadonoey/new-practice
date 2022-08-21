32f2c5696537b6ab192df0619b4d92df15bd6de0
Seoyeon Park <happy_tjdus@naver.com>
2022-08-21 23:24:22 +0900

    learned node.js

--- object.js
+++ object.js
@@ -1,1 +1,12 @@
 var skaterArray = ['sueun','chaeeun','juyeon','haeun'];
+console.log(skaterArray[1],"skaterArray[1]");
+
+var skaterObject = {
+    'Korea':'Yunah',
+    'Japan':'Rika',
+    'USA':'Alysa',
+    Russia:'Alena'
+};
+skaterObject.Korea = 'Yuna'
+console.log('skaterObject.USA', skaterObject.USA);
+console.log(skaterObject.Korea);
