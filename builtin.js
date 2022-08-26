a28bf8a7bf36be5c3697e62fe194b8d36c790b7f
Seoyeon Park <happy_tjdus@naver.com>
2022-08-27 03:21:27 +0900

    220826 공부

--- builtin.js
+++ builtin.js
@@ -1,2 +1,16 @@
 console.log(Math.PI);
 console.log(Math.random());
+
+var MyMath = {
+    PI:Math.PI,
+    random:function(){
+        return Math.random();
+    },
+    floor:function(val){
+        return Math.floor(val)
+    }
+}
+
+console.log('MyMath.PI',MyMath.PI);
+console.log('MyMath.random',MyMath.random());
+console.log('MyMath.PI',MyMath.floor(2.56));
