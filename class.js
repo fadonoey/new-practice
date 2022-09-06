b1819eeee407c0c256c420b32df8c31fcb6977ae
Seoyeon Park <happy_tjdus@naver.com>
2022-09-06 23:52:53 +0900

    상속을 배움

--- class.js
+++ class.js
@@ -3,13 +3,20 @@ class Person{
         this.name = name;
         this.first = first;
         this.second = second;
-        console.log('constructor');
+        
     }
 }
 
-var kim = new Person('kim', 20, 30);
+class PersonPlus extends Person{
+    avg(){
+        return (this.first+this.second)/2;
+    }
+}
+
+var kim = new PersonPlus('kim', 20, 30);
 
 console.log('kim', kim);
+console.log('kim avg', kim.avg());
 // kim.sum = function(){
 //     return 'kim: '+(this.first+this.second);
 // };
